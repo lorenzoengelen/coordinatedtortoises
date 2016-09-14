941e22274c5957c030c7dfcf6bff3361eb1b39c8
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-14 12:01:55 -0700

    (feat) added some comments

--- server/workers/serverSocket.js
+++ server/workers/serverSocket.js
@@ -5,6 +5,7 @@ var wss = new WebSocketServer({host: 'localhost', port: 4000});
 
 //Broadcasts all the bitcoin data to each client
 wss.broadcast = function(data) {
+	console.log(data);
   wss.clients.forEach(function(client) {
     client.send(data);
   });
@@ -15,3 +16,6 @@ wss.newConnection = function(cb) {
 };
 
 module.exports = wss;
+
+// {"bc":0.33302196,"time":1473875063000,"ip":"127.0.0.1","coords":[]}
+// {"bc":0.90153479,"time":1473875063000,"ip":"46.4.132.233","coords":[51.2993,9.491]}
