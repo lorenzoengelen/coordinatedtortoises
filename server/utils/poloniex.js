6a69856c5002202bb779da5e46045d0c07380358
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-15 16:05:21 -0700

    (feat) stream btcc

--- server/utils/poloniex.js
+++ server/utils/poloniex.js
@@ -7,38 +7,31 @@ var connection = new autobahn.Connection({
   realm: "realm1"
 });
 
+var newStream = function(data) {
+  data.forEach(function(element) {
+    if (element.type === 'newTrade') {
+    	return {
+        name: 'poloniex',
+        amount: element.data.amount,
+        lat: 39.7299628,
+        lon: -75.5645898,
+        time: element.data.date
+    	};
+    }
+  });
+};
+
 connection.onopen = function (session) {
 	var marketEvent = function(args, kwargs) {
-	  // console.log(args);
+	  var stream = newStream(args);
+	  if (stream) {
+		  server.broadcast(JSON.stringify(stream));
+	  }
 	}
 	session.subscribe('USDT_BTC', marketEvent);
-}
- 
-connection.onclose = function () {
-  console.log("Websocket connection closed");
 }
                        
 connection.open();
 
 server.newConnection(function(ws) {
-  console.log('POLONIEX is up');
 });
-
-// [ { type: 'orderBookModify',
-//     data: { type: 'ask', rate: '0.01608000', amount: '73.09309819' } },
-//   { type: 'newTrade',
-//     data:
-//      { amount: '3.01132825',
-//        date: '2016-09-15 17:59:27',
-//        rate: '0.01608000',
-//        total: '0.04842215',
-//        tradeID: '4921920',
-//        type: 'buy' } },
-//   { type: 'newTrade',
-//     data:
-//      { amount: '0.00757356',
-//        date: '2016-09-15 17:59:27',
-//        rate: '0.01608000',
-//        total: '0.00012178',
-//        tradeID: '4921921',
-//        type: 'buy' } } ]
