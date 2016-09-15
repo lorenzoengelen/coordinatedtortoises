12039cd72f840233779d1455d83ef75a9ba64818
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-15 16:17:33 -0700

    (feat) rocktrading stream

--- server/utils/rocktrading.js
+++ server/utils/rocktrading.js
@@ -4,26 +4,26 @@ var Pusher = require('pusher-js/node');
 
 var pusher = new Pusher('8458eb6fbd288f0cf3d8');
 var trades_channel = pusher.subscribe('currency');
+
+var newStream = function(data) {
+  if (data.symbol === 'BTCEUR') {
+  	return {
+      name: 'rocktrading',
+      amount: data.quantity,
+      lat: 35.9439061,
+      lon: 14.0998386,
+      time: data.time
+  	};
+  }
+};
+
 trades_channel.bind('last_trade', function(data) {
-  // console.log(data);
+  var stream = newStream(data);
+  if (stream) {
+  	console.log(stream);
+  }
 });
 
 server.newConnection(function(ws) {
   console.log('ROCKTRADING is up');
 });
-
-// { symbol: 'BTCEUR',
-//   quantity: 0.135,
-//   value: 542.5,
-//   volume: 73.23,
-//   time: '2016-09-15T20:25:05+02:00',
-//   diff: -0.33,
-//   dark: false }
-
-// { symbol: 'LTCBTC',
-//   quantity: 0.02,
-//   value: 0.00627,
-//   volume: 0.0001254,
-//   time: '2016-09-15T20:26:15+02:00',
-//   diff: -0.48,
-//   dark: false }
