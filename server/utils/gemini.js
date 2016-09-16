9ea01c6d939ecd99968c71be4403a7ffc6a41812
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-15 17:36:34 -0700

    (feat) rocktrading stream bug fix

--- server/utils/gemini.js
+++ server/utils/gemini.js
@@ -4,13 +4,33 @@ var server = require('../workers/serverSocket.js');
 
 var socket = new WebSocket('wss://api.gemini.com/v1/marketdata/btcusd');
 
+var newStream = function(data) {
+  if (data.events !== undefined) {
+	  data.events.forEach(function(event) {
+	    if (event.type === 'trade') {
+	    	console.log(event);
+	    	return {
+	    		name: 'gemini',
+	    		amount: event.amount,
+	    		lat: 40.8130495,
+	    		loc: -73.2549616,
+	    		time: new Date()
+	    	}
+	    }
+	  });
+  }
+};
+
 // subscribe to new transactions
 socket.onopen = function(event) {
 };
 
 socket.onmessage = function(event) {
-  var message = JSON.stringify(JSON.parse(event.data), undefined, 2);
-  // console.log(message);
+  var message = JSON.parse(event.data)
+  var stream = newStream(message);
+  if (stream) {
+	  server.broadcast(JSON.stringify(stream));
+  }
 };
 
 server.newConnection(function(ws) {
--- server/workers/serverSocket.js
+++ server/workers/serverSocket.js
@@ -5,7 +5,7 @@ var wss = new WebSocketServer({host: 'localhost', port: 4000});
 
 //Broadcasts all the bitcoin data to each client
 wss.broadcast = function(data) {
-	// console.log(data);
+	console.log(data);
   wss.clients.forEach(function(client) {
     client.send(data);
   });
